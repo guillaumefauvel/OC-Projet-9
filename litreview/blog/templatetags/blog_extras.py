2738d1ea7769d066c7cd6a49758aebdc54ae162a
Guillaume Fauvel <89651835+guillaumefauvel@users.noreply.github.com>
2022-02-10 10:14:22 +0100

    Ajout de la balise personnalisée add_paranthesis

--- litreview/blog/templatetags/blog_extras.py
+++ litreview/blog/templatetags/blog_extras.py
@@ -6,3 +6,11 @@ register = Library()
 @register.filter
 def model_type(value):
     return type(value).__name__
+
+
+@register.simple_tag(takes_context=True)
+def add_paranthesis(context, context2):
+    if context2:
+        return f'({context2})'
+    else:
+        return ''
